b55e442965e64122688e8e4d861c4b79232aa13f
Javier Orellana <javier.orellana2704@gmail.com>
2024-07-07 09:37:09 -0400

    add: Added zero downtime for ASG

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,9 +1,18 @@
 provider "aws" {
   region = "us-east-2"
+
+  # Tags to apply to all AWS resources by default
+  default_tags {
+    tags = {
+      Owner       = "DevOps Team"
+      Environment = "Stage"
+      ManagedBy   = "Terraform"
+    }
+  }
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "git@github.com:orellanaJav/terraform-modules.git//modules/services/webserver-cluster?ref=v0.0.6"
 
   cluster_name           = "webserver-stage"
   db_remote_state_bucket = "terraform-state-510693472221"
@@ -11,6 +20,15 @@ module "webserver_cluster" {
   instance_type          = "t2.micro"
   min_size               = 2
   max_size               = 2
+  enable_autoscaling     = false
+  server_text            = "Hola Mundo! mi guelita linda :D"
+
+  custom_tags = {
+    Owner       = "DevOps Team"
+    Environment = "Stage"
+    ManagedBy   = "Terraform"
+  }
+
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
